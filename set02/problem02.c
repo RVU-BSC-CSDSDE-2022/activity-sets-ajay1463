d66dc0fc08a72c97b2a026f313405971cc627175
Ajay  Hegde <115770268+ajay1463@users.noreply.github.com>
2022-12-14 09:17:09 +0000

    updated

--- set02/problem02.c
+++ set02/problem02.c
@@ -1,44 +1,39 @@
 #include <stdio.h>
-int input_side();
-int check_scalene(int a, int b, int c);
-void output(int a, int b, int c, int isscalene);
+
+typedef struct triangle
+{
+   float base;
+   float altitude;
+   float area;
+} Triangle;
+Triangle input_triangle();
+void find_area(Triangle *t);
+void output(Triangle t);
 
 int main()
 {
-  int a,b,c, isscalene;
-  a=input_side();
-  b=input_side();
-  c=input_side();
-  isscalene=check_scalene(a,b,c);
-  output(a,b,c,isscalene);
+   Triangle t;
+   t=input_triangle();
+   find_area(&t);
+   output(t);
+  return 0;
+}
+Triangle input_triangle()
+{
+    Triangle t;
+    printf("Enter the base of the triangle\n");
+  scanf("%f" , &t.base);
+  printf("Enter the altitude of the triangle\n");
+  scanf("%f", &t.altitude);
+  return t;
 }
 
-int input_side()
+void find_area(Triangle *t)
 {
-  int n;
-  printf("Enter number of sides");
-  scanf("%d",&n);
-  return n;
+      t->area=((t->base)*(t->altitude))*0.5;
 }
 
-int check_scalene(int a, int b, int c)
+void output(Triangle t)
 {
-  int isscalene;
-  if (a != b && b != c && c != a)
-    return isscalene;
-  else if (a==b && b==c)
-    return isscalene;
-  else if (a==b && a==c && b==c)
-    return isscalene;
-}
-void output(int a, int b, int c, int isscalene)
-{
-  if (a != b && b != c && c != a)
-  {
-    printf("The triangle with sides %d, %d and %d is scalene",a,b,c);
-  }
-  else
-  {
-    printf("The triangle with sides %d, %d and %d is not scalene",a,b,c);
-  }
+   printf("The area of the triangle is %f", t.area);
 }
