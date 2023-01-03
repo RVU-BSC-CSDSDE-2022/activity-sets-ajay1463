f45014b170027743a7d19b2c416fa4b732a4adbc
Ajay  Hegde <115770268+ajay1463@users.noreply.github.com>
2023-01-03 09:59:16 +0000

    updated

---
+++ set01/problem06.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int input();
+void compare(int a, int b, int c, int *largest);
+void output(int a, int b, int c, int largest);
+void main()
+{
+    int a,b,c,largest;
+    a=input();
+    b=input();
+    c=input();
+    compare(a,b,c,&largest);
+    output(a,b,c,largest);
+
+}
+int input()
+{
+    int num;
+    printf("Enter the number\n");
+    scanf("%d",&num);
+    return num;
+}
+void compare(int a, int b, int c, int *largest)
+{
+    if((a>=b)&(a>=c))
+    {
+        *largest=a;
+    }
+    else if((b>=a)&(b>=c))
+    {
+        *largest=b;
+    }
+    else
+    {
+        *largest=c;
+    }
+}
+void output(int a, int b, int c, int largest)
+{
+    printf("The largest of %d , %d and %d is %d",a,b,c,largest);
+}
---
+++ set01/problem07.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int input_n();
+int sum_n_nos(int n);
+void output(int n, int sum);
+void main()
+{
+    int n,sum;
+    n=input_n();
+    sum=sum_n_nos(n);
+    output(n,sum);
+}
+int input_n()
+{
+    int n;
+    printf("Enter the number n\n");
+    scanf("%d",&n);
+    return n;
+}
+int sum_n_nos(int n)
+{
+    int i,sum=0;
+    for(i=0;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+    return sum;
+}
+void output(int n, int sum)
+{
+    printf("The sum of %d natural numbers is %d",n,sum);
+}
