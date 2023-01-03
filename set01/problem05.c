2125c444a2b9dde9b30a45ed07146832a4e72e7f
Ajay  Hegde <115770268+ajay1463@users.noreply.github.com>
2023-01-03 05:49:06 +0000

    updated

---
+++ set01/problem05.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int input();
+int compare(int a, int b, int c);
+void output(int a, int b, int c, int largest);
+void main()
+{   
+    int a,b,c,largest;
+    a=input();
+    b=input();
+    c=input();
+    largest=compare(a,b,c);
+    output(a,b,c,largest);
+
+}
+int input()
+{   
+    int num;
+    printf("Enter the number\n");
+    scanf("%d",&num);
+    return num;
+}
+int compare(int a, int b, int c)
+{
+    int large;
+    if((a>=b)&(a>=c))
+    {
+        large=a;
+    }
+    else if ((b>=a)&(b>=c))
+    {
+        large=b;
+    }
+    else
+    {
+        large=c;
+    }
+    return large;
+}
+void output(int a, int b, int c, int largest)
+{
+    printf("The largest of %d , %d and %d is %d",a,b,c,largest);
+}
